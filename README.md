6930f2dd099f6115eec016231bc5f97fbddc4436
xx529 <575506806@qq.com>
2022-07-07 00:02:19 +0800

    test database connection

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ alg                    // 算法程序
 ctl                    // 后台程序
 web                    // 前端页面
 docker-compose.yaml    // 程序启动文件
+envfile.env            // 环境变量配置
 ```
 
 ## 2. 启动
--- app/web/start.py
+++ app/web/start.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import os
 
 if __name__ == '__main__':
 
@@ -6,3 +7,9 @@ if __name__ == '__main__':
     st.markdown('***')
     st.markdown('## 二级标题')
     st.markdown('### 三级标题')
+
+    st.write(os.getenv('POSTGRES_USER'))
+    st.write(os.getenv('POSTGRES_PASSWORD'))
+    st.write(os.getenv('PROJECT_DBNAME'))
+    st.write(os.getenv('PG_HOST'))
+    st.write(os.getenv('PG_PORT'))
